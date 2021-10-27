7111838b8205b165472c38139b2243107773462c
Taufan Prihantoro <taufan@airasiaredtix.com>
2021-10-27 15:36:40 +0700

    setup alias

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
   env: {
     production: {
-      plugins: ['react-native-paper/babel'],
+      plugins: ["react-native-paper/babel"],
     },
   },
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        alias: {
+          "@components": "./src/components/index.ts",
+        },
+      },
+    ],
+  ],
 };
