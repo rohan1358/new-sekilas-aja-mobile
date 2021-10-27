e5218127f2e6a370c89f85c954d4cff9b3de3e55
Taufan Prihantoro <taufan@airasiaredtix.com>
2021-10-27 12:16:37 +0700

    Login flow

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,6 +1,7 @@
+import Alert from "./Alert.svg";
 import Bell from "./Bell.svg";
 import Check from "./Check.svg";
 import Eye from "./Eye.svg";
 import EyeOff from "./EyeOff.svg";
 
-export { Bell, Check, Eye, EyeOff };
+export { Alert, Bell, Check, Eye, EyeOff };
--- src/constants/strings/index.ts
+++ src/constants/strings/index.ts
@@ -8,22 +8,33 @@ export const strings = {
   continueReading: "Lanjutkan membaca",
 
   doHaveAcc: "Sudah punya akun?",
+  dontHaveAcc: "Belum punya akun?",
 
+  emailAddress: "Alamat Email",
   emailCantBeEmpty: "Email tidak boleh kosong",
+  emailNotFound: "Email tidak ditemukan",
   emailPlaceholder: "Isi email disini ...",
+  emailUsed: "Email sudah terdaftar",
+  enterPassword: "Masukin Password",
+  enterConfirmPassword: "Konfirmasi Password",
+
+  forgotPassword: "Lupa password?",
 
   happyReading: "Selamat membaca",
 
   invalidEmail: "Email tidak valid",
 
+  login: "Masuk!",
   loginHere: "Login di sini",
 
+  name: "Nama",
   nameCantBeEmpty: "Nama tidak boleh kosong",
   nameMinChar: "Nama minimal berisi 3 karakter",
   namePlaceholder: "Isi nama disini ...",
 
   passwordCantBeEmpty: "Password tidak boleh kosong",
   passwordDontMatch: "Password tidak sama",
+  passwordInvalid: "Password tidak cocok",
   passwordMinChar: "Password minimal berisi 8 karakter",
   passwordPlaceholder: "Isi password disini ...",
   policy: "Kebijakan Privasi",
@@ -31,6 +42,7 @@ export const strings = {
   repasswordPlaceholder: "Konfirmasi password disini ...",
 
   regist: "Daftar!",
+  registHere: "Daftar di sini.",
 
   terms: "Ketentuan Layanan",
 
