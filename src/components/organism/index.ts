c26bf4e703b5d46b49b00d52e3907b45402460c8
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2021-11-03 22:02:43 +0700

    revisi

--- src/components/organism/index.ts
+++ src/components/organism/index.ts
@@ -3,6 +3,6 @@ import HomeHeader from "./HomeHeader";
 import ImageBanner from "./ImageBanner";
 import MiniCollectionTile from "./MiniCollectionTile";
 import OngoingTile from "./OngoingTile";
-import PagesOnboarding from "./OngoingTile";
+import PagesOnboarding from "./PageOnboarding"
 
 export { BookTile, HomeHeader, ImageBanner, MiniCollectionTile, OngoingTile, PagesOnboarding };
