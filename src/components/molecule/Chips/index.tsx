f54db8079f288bf051a7618ea348efef240a94ce
Taufan Prihantoro <taufan@airasiaredtix.com>
2021-11-05 10:02:24 +0700

    Animate chips

--- src/components/molecule/Chips/index.tsx
+++ src/components/molecule/Chips/index.tsx
@@ -1,8 +1,14 @@
-import { Fire } from "@assets";
-import { Button, Gap, TextItem } from "../../atom";
 import { neutralColor, primaryColor, spacing as sp } from "@constants";
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
+import { Button, Gap, TextItem } from "../../atom";
 import styles from "./styles";
 
 const Chips = ({
@@ -12,16 +18,35 @@ const Chips = ({
   label,
   Icon,
 }: ChipsProps) => {
+  const scale = useSharedValue(1);
+  const rotate = useSharedValue(0);
   const s = styles({ isSelected });
+
+  const animate = () => {
+    if (!isSelected) {
+      return;
+    }
+    scale.value = withRepeat(withTiming(1.25), 2, true);
+    rotate.value = withSequence(withTiming(-20), withTiming(20), withTiming(0));
+  };
+
+  const iconStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotate.value}deg` }],
+  }));
+
+  useEffect(() => {
+    animate();
+  }, [isSelected]);
+
   return (
     <Button style={s.container} onPress={() => onPress(id)}>
       <TextItem type={`r.16.${isSelected ? "pc.main" : "nc.70"}.c`}>
         {label}
       </TextItem>
       <Gap horizontal={sp.xs} />
-      <View style={s.icon}>
+      <Animated.View style={[s.icon, iconStyle]}>
         <Icon stroke={isSelected ? primaryColor.main : neutralColor[70]} />
-      </View>
+      </Animated.View>
     </Button>
   );
 };
