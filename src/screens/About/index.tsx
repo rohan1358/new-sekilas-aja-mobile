3ab4b6173a21b902faa37e7aaea5c548dd6ca0c3
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2021-11-10 09:12:09 +0700

    revisi page about

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,11 +1,17 @@
-import { AboutHeader, TextItem } from '../../components'
-import React from 'react'
+import { AboutHeader, Base, TextItem } from '../../components'
+import React, { useState } from 'react'
 import { View } from 'react-native';
 import styles from './styles'
+import { primaryColor, snackState as ss } from '@constants';
 
-export default function About({route, navigation}: AboutProps) {
+export default function About({ route, navigation }: AboutProps) {
+    const [snackState, setSnackState] = useState<SnackStateProps>(ss.closeState);
     return (
-        <View>
+        <Base
+            barColor={primaryColor.main}
+            snackState={snackState}
+            setSnackState={setSnackState}
+        >
             <AboutHeader
                 title={route.params.title}
                 navigation={navigation}
@@ -15,6 +21,6 @@ export default function About({route, navigation}: AboutProps) {
                 <TextItem style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque volutpat at dictum ut ullamcorper quisque porttitor. Tristique faucibus quam sit at massa. Sed hendrerit aliquet id senectus. Nunc tempor, dignissim nisl lectus enim mauris nunc condimentum. Facilisis elit elementum posuere posuere. Placerat risus venenatis eget gravida adipiscing malesuada. Sollicitudin cum tortor ac non quisque pellentesque sit.</TextItem>
                 <TextItem style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque volutpat at dictum ut ullamcorper quisque porttitor. Tristique faucibus quam sit at massa. Sed hendrerit aliquet id senectus. Nunc tempor, dignissim nisl lectus enim mauris nunc condimentum. Facilisis elit elementum posuere posuere. Placerat risus venenatis eget gravida adipiscing malesuada. Sollicitudin cum tortor ac non quisque pellentesque sit.</TextItem>
             </View>
-        </View>
+        </Base>
     )
 }
