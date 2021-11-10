438271fd8a41a274751e1cee7d995b2e361df363
Taufan Prihantoro <taufan@airasiaredtix.com>
2021-11-10 10:52:46 +0700

    Release internal 0.0.5

--- src/components/organism/AboutHeader/index.tsx
+++ src/components/organism/AboutHeader/index.tsx
@@ -1,18 +1,17 @@
-import { Button } from '../../../components'
-import React from 'react'
-import { Text, View } from 'react-native'
-import { ArrowLeft } from '@assets';
-import styles from './styles'
-import { TextItem } from '../../atom';
-import { neutralColor } from '@constants';
+import { ArrowLeft } from "@assets";
+import { neutralColor } from "@constants";
+import React from "react";
+import { View } from "react-native";
+import { Button, TextItem } from "../../atom";
+import styles from "./styles";
 
-export default function AboutHeader({title, navigation}: AboutHeaderProps) {
-    return (
-        <View style={styles.container}>
-            <Button onPress={()=> navigation.goBack()} style={styles.btnBack}>
-                <ArrowLeft color={neutralColor[90]} />
-            </Button>
-            <TextItem style={styles.title}>{title}</TextItem>
-        </View>
-    )
+export default function AboutHeader({ title, navigation }: AboutHeaderProps) {
+  return (
+    <View style={styles.container}>
+      <Button onPress={() => navigation.goBack()} style={styles.btnBack}>
+        <ArrowLeft color={neutralColor[90]} />
+      </Button>
+      <TextItem style={styles.title}>{title}</TextItem>
+    </View>
+  );
 }
--- src/components/organism/ProfileHeader/index.tsx
+++ src/components/organism/ProfileHeader/index.tsx
@@ -1,35 +1,32 @@
-import { Button, TextItem, Amage } from '../../../components'
-import React from 'react'
-import { Image, View } from 'react-native'
-import styles from './styles'
-import { ArrowLeft, BgHeader1, UserPlaceholder } from '@assets'
-import { neutralColor, strings } from '@constants'
+import { Button, TextItem, Amage } from "../../atom";
+import React from "react";
+import { Image, View } from "react-native";
+import styles from "./styles";
+import { ArrowLeft, BgHeader1, UserPlaceholder } from "@assets";
+import { neutralColor, strings } from "@constants";
 
-
-export default function ProfileHeader({
-    navigation,
-    uri,
-    onPress
-}: any) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.sectionMenu}>
-                <Button onPress={()=> navigation.goBack()}>
-                    <ArrowLeft color={neutralColor[90]} />
-                </Button>
-                <TextItem type='b.20.nc.90.c' style={styles.textMenu}>{strings.edit_profile}</TextItem>
-            </View>
-            <Button onPress={()=> onPress()} style={styles.sectionProfile}>
-                <View style={styles.profileContainer}>
-                    {!!uri ? (
-                    <Amage source={uri} />
-                    ) : (
-                    <Amage placeholder={UserPlaceholder} />
-                    )}
-                </View>
-                <TextItem type="b.24.nc.90.c">{strings.edit_profile}</TextItem>
-            </Button>
-            <Image source={BgHeader1} style={styles.bgHeader} />
+export default function ProfileHeader({ navigation, uri, onPress }: any) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.sectionMenu}>
+        <Button onPress={() => navigation.goBack()}>
+          <ArrowLeft color={neutralColor[90]} />
+        </Button>
+        <TextItem type="b.20.nc.90.c" style={styles.textMenu}>
+          {strings.edit_profile}
+        </TextItem>
+      </View>
+      <Button onPress={() => onPress()} style={styles.sectionProfile}>
+        <View style={styles.profileContainer}>
+          {!!uri ? (
+            <Amage source={uri} />
+          ) : (
+            <Amage placeholder={UserPlaceholder} />
+          )}
         </View>
-    )
+        <TextItem type="b.24.nc.90.c">{strings.edit_profile}</TextItem>
+      </Button>
+      <Image source={BgHeader1} style={styles.bgHeader} />
+    </View>
+  );
 }
