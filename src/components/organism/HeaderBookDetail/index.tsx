dc0d9a6f66cb14f71d6444f6161ae4c2675dceee
Taufan Prihantoro <taufan@airasiaredtix.com>
2021-11-21 07:41:37 +0700

    Release 0.0.6

--- src/components/organism/HeaderBookDetail/index.tsx
+++ src/components/organism/HeaderBookDetail/index.tsx
@@ -1,24 +1,32 @@
-import { ArrowLeft, Download, Heart, HeartBook } from '@assets'
-import { Button } from '@components'
-import { neutralColor } from '@constants'
-import React from 'react'
-import {Text, View } from 'react-native'
-import styles from './styles'
+import { ArrowLeft, Download, HeartBook } from "@assets";
+import { neutralColor } from "@constants";
+import React from "react";
+import { View } from "react-native";
+import { Button } from "../../atom";
+import styles from "./styles";
 
-export default function HeaderBookDetail({navigation, onDownload, onFavorite, Active}: HeaderBookDetailProps) {
-    return (
-        <View style={styles.container}>
-            <Button onPress={()=> navigation.goBack()}>
-                <ArrowLeft color={neutralColor[90]} />
-            </Button>
-            <View style={styles.boxRight}>
-                <Button onPress={()=> onDownload && onDownload()} style={styles.btn}>
-                    <Download color={neutralColor[90]} />
-                </Button>
-                <Button onPress={()=> onFavorite && onFavorite()} style={[styles.btnHeart, styles.btn]}>
-                    <HeartBook />
-                </Button>
-            </View>
-        </View>
-    )
+export default function HeaderBookDetail({
+  navigation,
+  onDownload,
+  onFavorite,
+  Active,
+}: HeaderBookDetailProps) {
+  return (
+    <View style={styles.container}>
+      <Button onPress={() => navigation.goBack()}>
+        <ArrowLeft color={neutralColor[90]} />
+      </Button>
+      <View style={styles.boxRight}>
+        <Button onPress={() => onDownload && onDownload()} style={styles.btn}>
+          <Download color={neutralColor[90]} />
+        </Button>
+        <Button
+          onPress={() => onFavorite && onFavorite()}
+          style={[styles.btnHeart, styles.btn]}
+        >
+          <HeartBook />
+        </Button>
+      </View>
+    </View>
+  );
 }
--- src/components/organism/HeaderNotification/index.tsx
+++ src/components/organism/HeaderNotification/index.tsx
@@ -1,18 +1,18 @@
-import { Button } from '@components'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { ArrowLeft } from '@assets'
-import { neutralColor, strings } from '@constants'
-import styles from './styles'
-import { TextItem } from '../../atom'
+import { ArrowLeft } from "@assets";
+import { Button } from "../../atom";
+import { neutralColor, strings } from "@constants";
+import React from "react";
+import { View } from "react-native";
+import { TextItem } from "../../atom";
+import styles from "./styles";
 
-export default function HeaderNotification({navigation}:any) {
-    return (
-        <View style={styles.container}>
-            <Button onPress={()=> navigation.goBack()} style={styles.btnBack}>
-                <ArrowLeft color={neutralColor[90]} />
-            </Button>
-            <TextItem type='b.20.nc.90'>{strings.notification}</TextItem>
-        </View>
-    )
+export default function HeaderNotification({ navigation }: any) {
+  return (
+    <View style={styles.container}>
+      <Button onPress={() => navigation.goBack()} style={styles.btnBack}>
+        <ArrowLeft color={neutralColor[90]} />
+      </Button>
+      <TextItem type="b.20.nc.90">{strings.notification}</TextItem>
+    </View>
+  );
 }
