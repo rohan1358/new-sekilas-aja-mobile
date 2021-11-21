95d23b90ad128beaeb375686043df1058d493254
Taufan Prihantoro <taufan@airasiaredtix.com>
2021-11-21 07:39:30 +0700

    Adjustment

--- src/constants/strings/index.ts
+++ src/constants/strings/index.ts
@@ -12,47 +12,40 @@ export const strings = {
   ambil_langsung: "Ambil langsung",
   audio_Account: "Pengaturan Audio",
 
+  baca: "Baca",
+  bahasa: "Pilihan Bahasa",
+  beri_ulasan: "Berikan Ulasanmu",
   bookCategory: "Kategori buku",
   bookCollections: "Koleksi buku",
   bookCollectionsDesc:
     "Koleksi sejumlah buku berdasarkan kategori yang kamu minati.",
   booksNotFound: "Kita ga menemukan buku yang kamu cari.",
-
-  button_onBoard_1: "Yuk mulai!",
   btnLewati: "Lewati",
   btnLanjut: "Lanjut",
-  bahasa: "Pilihan Bahasa",
-
+  button_onBoard_1: "Yuk mulai!",
   berlangganan_Account: "Berlangganan",
-
   btnBatal: "Batal berlangganan",
-
-  btn_kebijakan: "Kebijakan Privasi",
-  btn_ketentuan: "Ketentuan Layanan",
-  btn_berikan_ulasan: "Berikan Kami Ulasan",
-
-  btn_keluar: "Keluar",
-
-  btn_simpan: "Simpan",
   btn_batal: "Batal",
-
-  baca: "Baca",
-  beri_ulasan: "Berikan Ulasanmu",
-
+  btn_berikan_ulasan: "Berikan Kami Ulasan",
+  btn_kebijakan: "Kebijakan Privasi",
+  btn_keluar: "Keluar",
+  btn_ketentuan: "Ketentuan Layanan",
+  btn_simpan: "Simpan",
   buku_serupa: "Buku Serupa",
 
-  continueReading: "Lanjutkan membaca",
   camera_unavailable: "Kamera tidak tersedia di perangkat",
+  continueReading: "Lanjutkan membaca",
 
+  daftar_isi: "Daftar Isi",
+  data_kosong: "Data kosong",
   delete: "Hapus",
+  dengar: "Dengar",
   doHaveAcc: "Sudah punya akun?",
   dontHaveAcc: "Belum punya akun?",
   downloadedBooks: "Buku yang diunduh",
   download_Account: "Pengaturan Download",
-  data_kosong: "Data kosong",
-  dengar: "Dengar",
-  daftar_isi: "Daftar Isi",
 
+  edit_profile: "Edit Profile",
   emailAddress: "Alamat Email",
   emailCantBeEmpty: "Email tidak boleh kosong",
   emailNotFound: "Email tidak ditemukan",
@@ -61,8 +54,6 @@ export const strings = {
   enterPassword: "Masukin Password",
   enterConfirmPassword: "Konfirmasi Password",
   explore: "Eksplor",
-
-  edit_profile: "Edit Profile",
 
   favBooks: "Buku favoritku",
   findFavBook: "Cari Buku Favoritmu!",
@@ -73,34 +64,34 @@ export const strings = {
   ganti_foto: "Ganti Profil",
 
   happyReading: "Selamat membaca",
-  home: "Beranda",
   hapus_download: "Hapus Download",
+  home: "Beranda",
 
-  invalidEmail: "Email tidak valid",
   ingatkan: "Ingatkan saya pada pukul..",
+  invalidEmail: "Email tidak valid",
 
   jam: "Jam",
 
+  kategori: "Kategori",
+  kirim: "Kirim",
+  komentar: "Komentar",
   kualitas: "Kualitas Audio",
   kualitas_video: "Kualitas Video",
   kualitas_download: "Kualitas Download",
-  kategori: "Kategori",
-  komentar: "Komentar",
-  kirim: "Kirim",
 
-  library: "Bukuku",
-  login: "Masuk!",
-  loginHere: "Login di sini",
+  lalu: " lalu",
   lebih_banyak: "Lihat Lebih Banyak",
   lebih_sedikit: "Lihat Lebih Sedikit",
+  library: "Bukuku",
   lihat_semua: "Lihat Semua",
-  lalu: " lalu",
+  login: "Masuk!",
+  loginHere: "Login di sini",
 
-  mostRead: "Paling banyak dibaca",
   masa_Account: "Masa Berlaku Premium",
-  mode_offline: "Mode Offline",
-  mode_gelap: "Mode Gelap",
   menit: "Menit",
+  mode_gelap: "Mode Gelap",
+  mode_offline: "Mode Offline",
+  mostRead: "Paling banyak dibaca",
 
   name: "Nama",
   notif: "Notifikasi",
@@ -112,19 +103,13 @@ export const strings = {
   noBook: "Buku ga ditemukan!",
   notif_sistem: "Notifikasi Sistem",
   notif_promo: "Notifikasi Promo",
-
   notification: "Notifikasi",
-
   notif_empty: "Notifikasi kosong!",
 
   onBoard_1: "Males baca buku? Baca SekilasAja!",
-
   onBoard_2: "Membaca adalah habit orang sukses!",
-
   onBoard_3: "Udah nyerah sebelum membaca.",
-
   onBoard_4: "Belajar dimanapun dan kapanpun!",
-
   other: "kesalahan lain",
 
   passwordCantBeEmpty: "Password tidak boleh kosong",
@@ -138,14 +123,11 @@ export const strings = {
   pilih_bahasa: "Pilih bahasa yang kamu mengerti.",
   putar: "Putar Otomatis",
   password_minimal: "Password minimal harus berisi 8 karakter.",
-
   password_lama: "Password Lama",
   password_baru: "Password Baru",
   password_konfir: "Konfirmasi Password",
-
   password_lama_kosong: "Password lama kosong",
   password_tidak_sama: "Password tidak sama",
-
   permission: "Kamera tidak memiliki izin",
   pengingat: "Pengingat Harian",
   promo_voucher: "Promo & Voucher",
@@ -153,11 +135,9 @@ export const strings = {
   publikasi: "Negara publikasi : ",
   penulis: "Penulis",
   pembaca: " pembaca",
-
   promo_empty: "Promo kosong!",
 
   repasswordPlaceholder: "Konfirmasi password disini ...",
-
   recommendedBook: "Rekomendasi buku",
   regist: "Daftar!",
   registHere: "Daftar di sini.",
@@ -174,54 +154,43 @@ export const strings = {
     "Kamu dapat belajar dan membaca segala hal dalam satu aplikasi!",
 
   terms: "Ketentuan Layanan",
-
   trendingBook: "Trending buku",
-
-  weekNewCollection: "Kilas Baru minggu ini!",
-
   title_Account: "Pengaturan",
   tipeAkun_Account: "Tipe Akun",
-
   text_mode_offline:
     "Saat koneksimu buruk atau gak ada internet, kamu masih bisa menggunakan aplikasi dengan memilih mode offline.",
-
   text_notif: "Sesuaikan notifikasi.",
-
   text_putar: "Dengarkan ringkasan buku berikutnya secara otomatis.",
   text_kualitas:
     "Sesuaikan kualitas audio agar tetap mendengarkan dengan nyaman.",
-
-  video_Account: "Pengaturan Video",
-  video_putar: "Putar Otomatis",
   text_putar_video: "Tonton ringkasan buku berikutnya secara otomatis.",
   text_kualitas_video:
     "Sesuaikan kualitas video agar tetap menonton dengan nyaman.",
-
   text_kualitas_download:
     "Tentukan kualitas download ringkasan buku, untuk membuka ketika offline.",
   text_hapus_download: "Hapus semua ringkasan buku yang telah didownload.",
   text_btn_hapus: "Hapus",
-
   tentang: "Tentang",
   tentang_kami: "Tentang Kami",
   text_tentang:
     "Kepo sama kita? Yuk, liat yang ada dibelakang layar SekilasAja!",
-
   textSosmed: "Follow Sosial Media Kami",
-
-  nama: "Nama",
-  text_pengingat: "Kamu akan diingatkan setiap hari.",
-  text_promo: "Kamu akan diberitahu setiap ada promo diskon atau voucher.",
-  zona: "Zona",
-  wib: "WIB",
-
-  tonton: "Tonton",
-  tentang_buku: "Tentang Buku",
   tgl_release: "Tanggal Rilis",
-
   tidak_ada_notif: "Tidak ada notifikasi  untuk saat ini.",
   tidak_ada_promo: "Tidak ada promo  untuk saat ini.",
+  text_pengingat: "Kamu akan diingatkan setiap hari.",
+  text_promo: "Kamu akan diberitahu setiap ada promo diskon atau voucher.",
+  tonton: "Tonton",
+  tentang_buku: "Tentang Buku",
 
   ulasan: "Ulasan",
   ulasan_dari: "Ulasan dari ",
+
+  video_Account: "Pengaturan Video",
+  video_putar: "Putar Otomatis",
+
+  weekNewCollection: "Kilas Baru minggu ini!",
+  wib: "WIB",
+
+  zona: "Zona",
 };
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,23 +1,33 @@
-import { Base, Button, DummyFlatList, ProfileHeader, TextItem } from '../../components'
-import React, { useEffect, useRef, useState } from 'react'
-import { Modal, StyleSheet, Text, TextInput, View } from 'react-native'
-import SkeletonContent from 'react-native-skeleton-content-nonexpo';
-import { pages, primaryColor, skeleton, snackState as ss, strings } from '@constants';
-import styles from './styles'
-import { AlertModal, ChevronRight, EditGray } from '@assets';
-import { useDispatch, useSelector } from 'react-redux';
-import { ReduxState } from '../../redux/reducers';
+import {
+  Base,
+  Button,
+  DummyFlatList,
+  ProfileHeader,
+  TextItem,
+} from "../../components";
+import React, { useEffect, useRef, useState } from "react";
+import { Modal, StyleSheet, Text, TextInput, View } from "react-native";
+import SkeletonContent from "react-native-skeleton-content-nonexpo";
+import {
+  pages,
+  primaryColor,
+  skeleton,
+  snackState as ss,
+  strings,
+} from "@constants";
+import styles from "./styles";
+import { AlertModal, ChevronRight, EditGray } from "@assets";
+import { useDispatch, useSelector } from "react-redux";
+import { ReduxState } from "../../redux/reducers";
 import RBSheet from "react-native-raw-bottom-sheet";
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-import { loggingIn, setProfileRedux } from '../../redux/actions';
-import { CommonActions } from '@react-navigation/routers';
-
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
+import { loggingIn, setProfileRedux } from "../../redux/actions";
+import { CommonActions } from "@react-navigation/routers";
 
 export default function Profile({ navigation }: any) {
-
   const dispatch = useDispatch();
   const refRBSheet = useRef();
-  
+
   const {
     editProfile: { profile },
   } = useSelector((state: ReduxState) => state);
@@ -26,97 +36,104 @@ export default function Profile({ navigation }: any) {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [modalAlert, setModalAlert] = useState<boolean>(false);
   const [textAlert, setTextAlert] = useState({
-    text: '',
-    action: '',
-    button: '',
+    text: "",
+    action: "",
+    button: "",
   });
 
   // console.log(nama)
 
   const [imageUrl, setImageUrl] = useState(null);
-  const [nameState, setName] = useState<string>('');
-  const [emailState, setEmail] = useState<string>('');
+  const [nameState, setName] = useState<string>("");
+  const [emailState, setEmail] = useState<string>("");
 
   useEffect(() => {
     if (profile) {
-      setName(profile.firstName)
-      setEmail(profile.email)
+      setName(profile.firstName);
+      setEmail(profile.email);
     }
-  },[])
-
+  }, []);
 
   const handleImagePicker = (type: string) => {
-    if (type == 'camera') {
-      launchCamera({
-        mediaType: 'photo',
-        quality: 1,
-        saveToPhotos: true,
-        maxHeight: 500,
-        maxWidth: 500,
-        includeBase64: true
-      }, (callback) => {
-        if (callback.errorCode === 'camera_unavailable') {
-          setSnackState(ss.failState(strings.camera_unavailable));
-          return;
-        } else if (callback.errorCode === 'permission') {
-          setSnackState(ss.failState(strings.permission));
-          return;
-        } else if (callback.errorCode === 'others') {
-          setSnackState(ss.failState(strings.other));
-          return;
-        } else if (callback.didCancel) {
-          console.log('camera cancel')
-        } else {
-          const base64 = { uri: 'data:image/jpeg;base64,' + callback.assets[0].base64 };
-          setImageUrl(base64.uri)
-          refRBSheet.current.close()
+    if (type == "camera") {
+      launchCamera(
+        {
+          mediaType: "photo",
+          quality: 1,
+          saveToPhotos: true,
+          maxHeight: 500,
+          maxWidth: 500,
+          includeBase64: true,
+        },
+        (callback) => {
+          if (callback.errorCode === "camera_unavailable") {
+            setSnackState(ss.failState(strings.camera_unavailable));
+            return;
+          } else if (callback.errorCode === "permission") {
+            setSnackState(ss.failState(strings.permission));
+            return;
+          } else if (callback.errorCode === "others") {
+            setSnackState(ss.failState(strings.other));
+            return;
+          } else if (callback.didCancel) {
+            console.log("camera cancel");
+          } else {
+            const base64 = {
+              uri: "data:image/jpeg;base64," + callback.assets[0].base64,
+            };
+            setImageUrl(base64.uri);
+            refRBSheet.current.close();
+          }
         }
-      });
+      );
     } else {
-      launchImageLibrary({
-        mediaType: 'photo',
-        quality: 1,
-        maxHeight: 500,
-        maxWidth: 500,
-        includeBase64: true
-      }, (callback) => {
-        if (callback.errorCode === 'camera_unavailable') {
-          setSnackState(ss.failState(strings.camera_unavailable));
-          return;
-        } else if (callback.errorCode === 'permission') {
-          setSnackState(ss.failState(strings.permission));
-          return;
-        } else if (callback.errorCode === 'others') {
-          setSnackState(ss.failState(strings.other));
-          return;
-        } else if (callback.didCancel) {
-          console.log('camera cancel')
-        } else {
-          const base64 = { uri: 'data:image/jpeg;base64,' + callback.assets[0].base64 };
-          setImageUrl(base64.uri)
-          refRBSheet.current.close()
+      launchImageLibrary(
+        {
+          mediaType: "photo",
+          quality: 1,
+          maxHeight: 500,
+          maxWidth: 500,
+          includeBase64: true,
+        },
+        (callback) => {
+          if (callback.errorCode === "camera_unavailable") {
+            setSnackState(ss.failState(strings.camera_unavailable));
+            return;
+          } else if (callback.errorCode === "permission") {
+            setSnackState(ss.failState(strings.permission));
+            return;
+          } else if (callback.errorCode === "others") {
+            setSnackState(ss.failState(strings.other));
+            return;
+          } else if (callback.didCancel) {
+            console.log("camera cancel");
+          } else {
+            const base64 = {
+              uri: "data:image/jpeg;base64," + callback.assets[0].base64,
+            };
+            setImageUrl(base64.uri);
+            refRBSheet.current.close();
+          }
         }
-      })
+      );
     }
-  }
+  };
 
   const navToEditProfile = (data: any) => {
-    navigation.navigate(pages.PageEditProfile, {data: data})
-  }
+    navigation.navigate(pages.PageEditProfile, { data: data });
+  };
 
   const logOut = () => {
     setModalAlert(!modalAlert);
-    dispatch(loggingIn({ isLogin: false, email: '' }));
-    dispatch(setProfileRedux(null))
+    dispatch(loggingIn({ isLogin: false, email: "" }));
+    dispatch(setProfileRedux(null));
     navigation.dispatch(
       CommonActions.reset({
         index: 1,
-        routes: [
-          { name: pages.SignIn },
-        ],
+        routes: [{ name: pages.SignIn }],
       })
     );
-  }
+  };
 
   return (
     <Base
@@ -125,9 +142,9 @@ export default function Profile({ navigation }: any) {
       setSnackState={setSnackState}
     >
       <SkeletonContent
-          containerStyle={styles.skeleton}
-          isLoading={isLoading}
-          layout={skeleton.mainHome}
+        containerStyle={styles.skeleton}
+        isLoading={isLoading}
+        layout={skeleton.mainHome}
       >
         <DummyFlatList>
           <ProfileHeader
@@ -136,8 +153,17 @@ export default function Profile({ navigation }: any) {
             uri={imageUrl}
           />
           <View style={styles.content}>
-            <TextItem style={styles.title}>{strings.nama}</TextItem>
-            <Button onPress={()=> navToEditProfile({type: 'nama', title: 'Ubah Nama', valueParams: nameState})} style={styles.boxItem}>
+            <TextItem style={styles.title}>{strings.name}</TextItem>
+            <Button
+              onPress={() =>
+                navToEditProfile({
+                  type: "nama",
+                  title: "Ubah Nama",
+                  valueParams: nameState,
+                })
+              }
+              style={styles.boxItem}
+            >
               <TextInput
                 style={styles.textInput}
                 editable={false}
@@ -146,33 +172,50 @@ export default function Profile({ navigation }: any) {
               <EditGray />
             </Button>
             <TextItem style={styles.title}>{strings.alamat}</TextItem>
-            <Button onPress={()=> navToEditProfile({type: 'email', title: 'Ubah Email', valueParams: emailState})} style={styles.boxItem}>
+            <Button
+              onPress={() =>
+                navToEditProfile({
+                  type: "email",
+                  title: "Ubah Email",
+                  valueParams: emailState,
+                })
+              }
+              style={styles.boxItem}
+            >
               <TextInput
                 style={styles.textInput}
                 editable={false}
                 value={emailState}
-                keyboardType='email-address'
+                keyboardType="email-address"
               />
               <EditGray />
             </Button>
             <TextItem style={styles.title}>{strings.password}</TextItem>
-            <Button onPress={()=> navToEditProfile({type: 'password', title: 'Ubah Password'})} style={styles.boxItem}>
+            <Button
+              onPress={() =>
+                navToEditProfile({ type: "password", title: "Ubah Password" })
+              }
+              style={styles.boxItem}
+            >
               <TextInput
                 style={styles.textInput}
                 editable={false}
-                value={emailState+'sj897'}
+                value={emailState + "sj897"}
                 secureTextEntry={true}
               />
               <EditGray />
             </Button>
-            <Button onPress={() => {
-              setModalAlert(true)
-              setTextAlert({
-                action: 'Cancel',
-                text: 'Yakin ingin keluar?',
-                button:'Keluar'
-              })
-            }} style={styles.btnKeluar}>
+            <Button
+              onPress={() => {
+                setModalAlert(true);
+                setTextAlert({
+                  action: "Cancel",
+                  text: "Yakin ingin keluar?",
+                  button: "Keluar",
+                });
+              }}
+              style={styles.btnKeluar}
+            >
               <TextItem style={styles.title}>{strings.btn_keluar}</TextItem>
             </Button>
           </View>
@@ -189,17 +232,27 @@ export default function Profile({ navigation }: any) {
           container: {
             borderTopLeftRadius: 24,
             borderTopRightRadius: 24,
-          }
+          },
         }}
         height={165}
       >
         <View style={styles.contaonerSheet}>
-          <TextItem style={[styles.title, styles.titleGantiFoto]}>{strings.ganti_foto}</TextItem>
-          <Button onPress={()=> handleImagePicker('camera')} style={styles.btnTakeAction}>
-            <TextItem style={styles.textTake}>{strings.ambil_langsung}</TextItem>
+          <TextItem style={[styles.title, styles.titleGantiFoto]}>
+            {strings.ganti_foto}
+          </TextItem>
+          <Button
+            onPress={() => handleImagePicker("camera")}
+            style={styles.btnTakeAction}
+          >
+            <TextItem style={styles.textTake}>
+              {strings.ambil_langsung}
+            </TextItem>
             <ChevronRight />
           </Button>
-          <Button onPress={()=> handleImagePicker('galery')} style={styles.btnTakeAction}>
+          <Button
+            onPress={() => handleImagePicker("galery")}
+            style={styles.btnTakeAction}
+          >
             <TextItem style={styles.textTake}>{strings.ambil_galery}</TextItem>
             <ChevronRight />
           </Button>
@@ -218,20 +271,30 @@ export default function Profile({ navigation }: any) {
             <DummyFlatList>
               <View style={styles.boxContentAlert}>
                 <AlertModal />
-                <TextItem style={styles.textAlert} >{textAlert.text}</TextItem>
+                <TextItem style={styles.textAlert}>{textAlert.text}</TextItem>
               </View>
-              <Button onPress={()=> setModalAlert(!modalAlert)} style={styles.btnAlert}>
-                <TextItem style={styles.textActionAlert} >{textAlert.action}</TextItem>
+              <Button
+                onPress={() => setModalAlert(!modalAlert)}
+                style={styles.btnAlert}
+              >
+                <TextItem style={styles.textActionAlert}>
+                  {textAlert.action}
+                </TextItem>
               </Button>
-              <Button onPress={() => {
-                logOut()
-              }} style={[styles.btnAlert, styles.btnAlertSecond]}>
-                <TextItem style={styles.textButtonAlert} >{textAlert.button}</TextItem>
+              <Button
+                onPress={() => {
+                  logOut();
+                }}
+                style={[styles.btnAlert, styles.btnAlertSecond]}
+              >
+                <TextItem style={styles.textButtonAlert}>
+                  {textAlert.button}
+                </TextItem>
               </Button>
             </DummyFlatList>
           </View>
         </View>
       </Modal>
     </Base>
-  )
+  );
 }
