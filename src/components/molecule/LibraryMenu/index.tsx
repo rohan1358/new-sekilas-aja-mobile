ee646ce6394cd0706144d6b43a12fc669b7f65c1
rohan <indo.office10@kasumi.co.jp>
2022-01-02 08:06:01 +0700

    handle favorit book -

--- src/components/molecule/LibraryMenu/index.tsx
+++ src/components/molecule/LibraryMenu/index.tsx
@@ -5,16 +5,16 @@ import { View } from "react-native";
 import styles from "./styles";
 import { neutralColor, spacing as sp } from "@constants";
 
-const LibraryMenu = ({ title, bookCount, icon,  action}: LibraryMenuProps) => {
+const LibraryMenu = ({ title, bookCount, icon, action }: LibraryMenuProps) => {
   return (
-    <Button onPress={()=>action && action()} style={styles.container}>
+    <Button onPress={() => action && action()} style={styles.container}>
       <View style={styles.iconContainer}>{icon}</View>
       <Gap horizontal={sp.xs} />
       <View style={styles.detail}>
         <TextItem type="b.16.nc.90" numberOfLines={1}>
           {title}
         </TextItem>
-        <Seed label={`${bookCount} Buku`} />
+        <Seed label={`${bookCount || 0} Buku`} />
       </View>
       <ChevronRight stroke={neutralColor[50]} />
     </Button>
