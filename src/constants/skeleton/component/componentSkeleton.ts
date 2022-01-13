3f5edbb36d1f44ffa8ea43b305155a863f70d20a
rohan <indo.office10@kasumi.co.jp>
2022-01-14 06:33:14 +0700

    replace spinner to skeleton

--- src/constants/skeleton/component/componentSkeleton.ts
+++ src/constants/skeleton/component/componentSkeleton.ts
@@ -4,14 +4,14 @@ import { widthPercent } from "../../../helpers";
 const componentCategory: any = [
   {
     key: "chip Category 1",
-    width: widthPercent(80),
+    width: widthPercent(60),
     height: 32,
     marginLeft: sp.sl,
     marginBottom: sp.sm
   },
   {
     key: "chip Category 2",
-    width: widthPercent(80),
+    width: widthPercent(50),
     height: 32,
     marginLeft: sp.sl,
     marginBottom: sp.sm
