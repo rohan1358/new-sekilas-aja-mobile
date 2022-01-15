f16ed194b3e73b6f593fdaca22c0a22db259e798
Joy Lodra <joylodra@gmail.com>
2022-01-14 21:03:22 -0800

    changed price

--- src/screens/Subscribe/dummy.ts
+++ src/screens/Subscribe/dummy.ts
@@ -1,16 +1,15 @@
 const paketList = [
-    {
-        type: 'best',
-        mount: 12,
-        hemat: 249000,
-        harga: 25000,
-    },
-    {
-        type: 'normal',
-        mount: 3,
-        harga: 35000,
-    },
-
-]
+  {
+    type: "best",
+    mount: 12,
+    hemat: 180000,
+    harga: 35000,
+  },
+  {
+    type: "normal",
+    mount: 3,
+    harga: 50000,
+  },
+];
 
 export default paketList;
